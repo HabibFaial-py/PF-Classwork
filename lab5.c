679dd3892b965d6e70d4e589b44b1d343dec7c42
HabibFaial-py <habibfaisaljandira@gmail.com>
2025-09-15 10:32:02 +0500

    Modified

--- lab5.c
+++ lab5.c
@@ -1,2 +1,23 @@
-#inlcude <stdio.h>
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char degree[10];
+	char result[10];
+	
+	printf("Do u have a masters degree?\n");
+	scanf("%s", degree);
+	if (strcmp(degree, "yes")==0){
+		printf("enter ur result");
+		scanf("%s",result);
+		if (strcmp(result,"pass")==0){
+			printf("You are hired");
+			}
+			else{
+				printf("U did not get the job");
+			}
+	}
+	else{
+		printf("You cannot apply");
+	}
+}
 
